5da6108ae36f8f4fa77a14217d3f0620d2f5274b
SpigotBypass <spigotbypass@gmail.com>
2025-10-18 16:51:32 +0200

    Update VanillaKnockbackPlus.java

--- src/main/java/dev/spigotbypass/vanillakbplus/VanillaKnockbackPlus.java
+++ src/main/java/dev/spigotbypass/vanillakbplus/VanillaKnockbackPlus.java
@@ -1,19 +1,35 @@
 package dev.spigotbypass.vanillakbplus;
+
 import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
-public class VanillaKnockbackPlus extends JavaPlugin{
-    public enum Mode{PVP,ALL}
-    private Mode mode;
-    @Override public void onEnable(){
-        saveDefaultConfig();
-        FileConfiguration c=getConfig();
-        mode="all".equalsIgnoreCase(c.getString("mode","pvp"))?Mode.ALL:Mode.PVP;
-        getServer().getPluginManager().registerEvents(new KBListener(this),this);
-        Bukkit.getLogger().info("VanillaKnockbackPlus enabled (mode="+mode+")");
+
+public class VanillaKnockbackPlus extends JavaPlugin {
+
+    public enum Mode {
+        PVP,
+        ALL
     }
-    @Override public void onDisable(){
+
+    private Mode mode;
+
+    @Override
+    public void onEnable() {
+        // Zapisz domyślny config jeśli nie istnieje
+        saveDefaultConfig();
+        FileConfiguration config = getConfig();
+
+        mode = "all".equalsIgnoreCase(config.getString("mode", "pvp")) ? Mode.ALL : Mode.PVP;
+        getServer().getPluginManager().registerEvents(new KBListener(this), this);
+        Bukkit.getLogger().info("VanillaKnockbackPlus enabled (mode=" + mode + ")");
+    }
+
+    @Override
+    public void onDisable() {
         Bukkit.getLogger().info("VanillaKnockbackPlus disabled");
     }
-    public Mode getMode(){return mode;}
+
+    public Mode getMode() {
+        return mode;
+    }
 }
